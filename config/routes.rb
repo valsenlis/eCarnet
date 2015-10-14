c31fbebba72ec40c1bee30486f8dbf93c6de1121
valsenlis <valsenlis@gmail.com>
2015-10-14 18:40:27 +0200

    Premier Workshop terminé

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +57,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+root 'home#index'
 end
+
+
